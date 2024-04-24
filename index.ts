a5a5e2b2fa0613b883639ab1157d4b3ba0f8a80d
Erum Naz <emmonaz2396@gmail.com>
2024-04-24 22:06:00 +0500

    Initial commit

---
+++ index.ts
@@ -0,0 +1,54 @@
+
+import inquirer from "inquirer"
+
+let myBalance=20000;
+let myPin=2396;
+let pinAns= await inquirer.prompt
+    (
+    [
+    {
+        name:"pin",
+        message:"please enter your pin",
+        type:"number",
+
+    }
+ 
+   ]
+   );
+   if(pinAns.pin===myPin)
+    console.log("correct pincode");
+
+   let operationAns= await inquirer.prompt
+   (
+  [
+  {
+    name:"operation",
+    message:"what do you want",
+    type:"list",
+    choices:["check Balance" , "withdraw"],
+
+   }
+    ]
+    );
+
+if(operationAns.operation==="check Balance")
+    console.log(`your current balance is: ${myBalance} in rupees `);
+
+
+  let amountAns = await inquirer.prompt
+(
+    [
+        {
+            name: "amount",
+            message:"enter your amount",
+            type: "number",
+
+
+
+}
+]
+);
+
+if(operationAns.operation==="withdraw")
+  console.log(amountAns.amount);
+
